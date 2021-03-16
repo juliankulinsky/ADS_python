6fe3deb983b582b2805e77e4d86ddf1a901e640f
kulinsky <juliankulinsky@web.de>
2021-03-16 18:51:33 +0100

    Initial Commit for MergeSort

---
+++ MergeSort/merge_sort.py
@@ -0,0 +1,12 @@
+def merge_sort(arrayA, arrayB):
+    """
+    Merge two sorted arrays A and B and sort it in a new array C
+
+    >>> merge_sort([3, 7, 92, 98], [4, 8, 72, 78])
+    [3, 4, 7, 8, 72, 78, 92, 98]
+    """
+
+
+
+
+
