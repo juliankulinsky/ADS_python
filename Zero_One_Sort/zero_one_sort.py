0ecc3f0abb89d5e8d33fe1fe11e577732e08cdd6
kulinsky <juliankulinsky@web.de>
2021-03-25 17:52:00 +0100

    Zero_One_Sort completed

---
+++ Zero_One_Sort/zero_one_sort.py
@@ -0,0 +1,22 @@
+def zero_one_sort(array):
+    """
+    Sort the given array, assuming that its entries are only 0 and 1.
+
+    >>> zero_one_sort([0, 0, 1, 0, 1, 0, 0, 1])
+    [0, 0, 0, 0, 0, 1, 1, 1]
+    """
+
+    #Count the number of 1s.
+    num_ones = 0
+    for x in array:
+        num_ones += x
+    num_zeroes = len(array) - num_ones
+
+    #Output num_zeroes 0s followed by num_ones 1s
+    result = []
+    for i in range(0, num_zeroes):
+        result.append(0)
+    for i in range(0, num_ones):
+        result.append(1)
+    return result
+
